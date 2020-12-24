b84dae3b0e338d58b1c78176d37b494ca9ee8624
Marcelo Almeida <contato@marceloalmeida.dev>
2020-12-24 10:31:14 -0300

    commented the install prompt

--- main.js
+++ main.js
@@ -10,37 +10,37 @@ if ('serviceWorker' in navigator) {
     })
 }
 
-let installPrompt;
+// let installPrompt;
 
-const button = document.querySelector('installButton');
-// hide the button initially
-button.style.display = 'none';
+// const button = document.querySelector('installButton');
+// // hide the button initially
+// button.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', e => {
-    e.preventDefault();
-    // show the button if it makes sense
-    button.style.display = 'block';
-    installPrompt = e;
-})
+// window.addEventListener('beforeinstallprompt', e => {
+//     e.preventDefault();
+//     // show the button if it makes sense
+//     button.style.display = 'block';
+//     installPrompt = e;
+// })
 
-button.addEventListener('click', () => {
-    if (!installPrompt) {
-        // The deferred prompt isn't available.
-        return;
-    }
+// button.addEventListener('click', () => {
+//     if (!installPrompt) {
+//         // The deferred prompt isn't available.
+//         return;
+//     }
 
-    // Show the install prompt.
-    installPrompt.prompt();
+//     // Show the install prompt.
+//     installPrompt.prompt();
 
-    // Log the result
-    installPrompt.userChoice.then((result) => {
-        console.log('userChoice', result);
-        // Reset the deferred prompt variable, since prompt() can only be called once.
-        installPrompt = null
-        // Hide the install button.
-        button.style.display = 'none';
-    })
-})
+//     // Log the result
+//     installPrompt.userChoice.then((result) => {
+//         console.log('userChoice', result);
+//         // Reset the deferred prompt variable, since prompt() can only be called once.
+//         installPrompt = null
+//         // Hide the install button.
+//         button.style.display = 'none';
+//     })
+// })
 
 if (navigator.share) {
     navigator.share({
